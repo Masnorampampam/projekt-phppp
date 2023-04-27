a89ce1dd1a91961d2f886a118bd7c64035bd4d23
Mateusz <mateuszkreft6@gmail.com>
2023-04-28 00:30:50 +0200

    dosc mam tego

--- cms/src/config.php
+++ cms/src/config.php
@@ -1,11 +1,9 @@
 <?php
 require_once('./../vendor/autoload.php');
-$db = new mysqli("localhost", "root", "", "meme");
+$db = new mysqli("localhost", "root", "", "cms");
 require("Post.class.php");
 require("User.class.php");
 require("Vote.class.php");
-//loader to taki pomocnik do ładowania szablon
 $loader = new Twig\Loader\FilesystemLoader("./../src/templates");
-//inicjujemy twiga
 $twig = new Twig\Environment($loader);
 ?>
