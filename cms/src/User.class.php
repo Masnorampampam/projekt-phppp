ce067031497624e9c98c026b54cd9765d24c2537
Mateusz <mateuszkreft6@gmail.com>
2023-05-09 02:13:10 +0200

    bebebaba

--- cms/src/User.class.php
+++ cms/src/User.class.php
@@ -1,25 +1,20 @@
 <?php
 class User {
-    //id użytkownika w bazie danych
     private int $id;
-    //login / email użytkownika w bazie danych
     private string $email;
 
-    //konstruktor
     public function __construct(int $id, string $email) {
         $this->id = $id;
         $this->email = $email;
 
     }
 
-    //gettery
     public function getId() : int {
         return $this->id;
     }
     public function getName() : string {
         return $this->email;
     }
-    //pobieranie nazwy użytkownika według jego id - potrzebne do wyświetlania autorów postów
     public static function getNameById(int $userId) : string {
         global $db;
         $query = $db->prepare("SELECT email FROM user WHERE id = ? LIMIT 1");
@@ -29,7 +24,6 @@ class User {
         $row = $result->fetch_assoc();
         return $row['email'];
     }
-    //zarejestrowanie nowego użytkownika
     public static function register(string $email, string $password) : bool {
         global $db;
         $query = $db->prepare("INSERT INTO user VALUES (NULL, ?, ?)");
@@ -37,7 +31,6 @@ class User {
         $query->bind_param('ss', $email, $passwordHash);
         return $query->execute();
     }
-    //zalogowanie istniejącego użytkownika
     public static function login(string $email, string $password) : bool {
         global $db;
         $query = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
@@ -46,9 +39,7 @@ class User {
         $result = $query->get_result();
         $row = $result->fetch_assoc();
         $passwordHash = $row['password'];
-        //jeżeli autoryzacja się powiedzie to zapisz użytkownika jako obiekt w sesji
         if(password_verify($password, $passwordHash)) {
-            //hasła są zgodne - możemy zalogować użytkownika
             $u = new User($row['id'], $email);
             $_SESSION['user'] = $u;
             return true;
@@ -57,7 +48,6 @@ class User {
         }
     }
     public static function isAuth() : bool {
-        //funkcja zwraca true jeśli użytkownik jest zalogowany
         if(isset($_SESSION['user'])) {
             if($_SESSION['user'] instanceof User) {
                 return true;
--- cms/src/Vote.class.php
+++ cms/src/Vote.class.php
@@ -1,15 +1,7 @@
 <?php
 class Vote {
-    /*
-    Deklarujemy sobie w bazie tabelę, 4 kolumny - id, post_id, value, user_id gdzie:
-    id - int autoincrement
-    post_id - int, klucz zewnętrzny - post do którego należy głos
-    score - int (-1,1)
-    user_id - int, klucz obcy do user.id - kto głosował
-    */
 
     public static function upVote(int $postId, int $userId) : bool {
-        //kod do dodawania upvotów
         global $db;
         $query = $db->prepare("INSERT INTO vote VALUES (NULL, ?, 1, ?)");
         $query->bind_param('ii', $postId, $userId);
@@ -27,7 +19,6 @@ class Vote {
     }
     public static function getScore(int $postId) : int {
         global $db;
-        //zwróć sumę głosów dla danego posta
         $query = $db->prepare("SELECT SUM(score) FROM vote WHERE post_id = ?");
         $query->bind_param('i', $postId);
         if($query->execute()){
@@ -35,7 +26,6 @@ class Vote {
             if($result->num_rows > 0) {
                 $row = $result->fetch_row();
                 if($row[0] == NULL) {
-                    //zwracane jeśli nie ma oddanych głosów
                     return 0;
                 } else 
                     return $row[0];
@@ -46,7 +36,6 @@ class Vote {
         return 0;
     }
     public static function getVote(int $postId, int $userId) : int {
-        //funkcja zwraca (-1,0,1) w zależności od teo czy użytkownik oddał już głos na tego mema
         global $db;
         $query = $db->prepare("SELECT score FROM vote WHERE post_id = ? AND user_id = ?");
         $query->bind_param('ii', $postId, $userId);
